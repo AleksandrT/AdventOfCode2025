efd57e8fa4a77dd8336b3dffdc7f3921e8b03221
Aleksandr Nexer <Aleksandr.Tsuranov@nexergroup.com>
2025-12-24 11:25:14 +0100

    Add project files.

---
+++ caAdventOfCode/Day1/SecretEntrancePartOne.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caAdventOfCode.Day1
+{
+    public class SecretEntrancePartOne
+    {
+        public SecretEntrancePartOne()
+        {
+            Console.WriteLine("Day 1: Secret Entrance - Part One");
+        }
+    }
+}
---
+++ caAdventOfCode/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var secretEntrancePartOne = new caAdventOfCode.Day1.SecretEntrancePartOne();
+
+Console.ReadLine();
+//Console.WriteLine( % 99);
